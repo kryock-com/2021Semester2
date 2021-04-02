c015ca330019a8d3816e0aa130335950699b070f
kryock-com <kevin.yockey@evsck12.com>
2021-04-02 10:03:31 -0500

    update

---
+++ Board Game Turtle Version/turtleMinesweeper.py
@@ -0,0 +1,25 @@
+import turtle as t
+import random as rand
+#------------VARABLS--------------------------------
+fontSetting=font=("System", 50, "normal")
+wn = t.Screen()
+wn.bgcolor("Black")
+wn.setup(width=300,height=300)
+
+
+#------------FUNCTIONS------------------
+mainMenu = t.Turtle()
+
+
+
+
+
+
+
+
+
+
+
+
+wn.mainloop()
+
